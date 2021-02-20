4f489c511fbad272c45e9f953f5ed1f1d46de37c
Douglas Motta <tibeca@gmail.com>
2021-02-20 19:37:52 +0000

    Skeleton

--- app/src/main/java/com/example/mergeadapterrecyclerviewtutorial/ProgrammingLanguage.kt
+++ app/src/main/java/com/example/mergeadapterrecyclerviewtutorial/ProgrammingLanguage.kt
@@ -1,15 +1,20 @@
 package com.example.mergeadapterrecyclerviewtutorial
 
+import androidx.annotation.DrawableRes
+
 data class ProgrammingLanguage(
     val name: String,
-    val paradigm: String
+    val paradigm: String,
+    @DrawableRes
+    val logo: Int
 )
 
 val programmingLanguages = listOf(
-    ProgrammingLanguage("Kotlin", "Orientação a Objetos, Funcional"),
-    ProgrammingLanguage("Java", "Orientação a Objetos"),
-    ProgrammingLanguage("Swift", "Orientação a Objetos, Funcional"),
-    ProgrammingLanguage("PHP", "Orientação a Objetos"),
-    ProgrammingLanguage("Python", "Orientação a Objetos"),
-    ProgrammingLanguage("C#", "Orientação a Objetos")
+    ProgrammingLanguage("Kotlin", "Orientação a Objetos, Funcional", R.drawable.ic_logo_kotlin),
+    ProgrammingLanguage("Java", "Orientação a Objetos", R.drawable.ic_logo_java),
+    ProgrammingLanguage("JavaScript", "Orientação a Objetos", R.drawable.ic_logo_javascript),
+    ProgrammingLanguage("Swift", "Orientação a Objetos, Funcional", R.drawable.ic_logo_swift),
+    ProgrammingLanguage("PHP", "Orientação a Objetos", R.drawable.ic_logo_php),
+    ProgrammingLanguage("Python", "Orientação a Objetos", R.drawable.ic_logo_python),
+    ProgrammingLanguage("C#", "Orientação a Objetos", R.drawable.ic_c_sharp)
 )
