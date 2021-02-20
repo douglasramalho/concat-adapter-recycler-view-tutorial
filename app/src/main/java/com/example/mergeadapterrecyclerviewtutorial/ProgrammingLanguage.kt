444ce9ef655b3003168ef35d9a91857ec2e21e9c
Douglas Motta <tibeca@gmail.com>
2021-02-20 18:26:11 +0000

    Skeleton

--- app/src/main/java/com/example/mergeadapterrecyclerviewtutorial/ProgrammingLanguage.kt
+++ app/src/main/java/com/example/mergeadapterrecyclerviewtutorial/ProgrammingLanguage.kt
@@ -4,3 +4,12 @@ data class ProgrammingLanguage(
     val name: String,
     val paradigm: String
 )
+
+val programmingLanguages = listOf(
+    ProgrammingLanguage("Kotlin", "Orientação a Objetos, Funcional"),
+    ProgrammingLanguage("Java", "Orientação a Objetos"),
+    ProgrammingLanguage("Swift", "Orientação a Objetos, Funcional"),
+    ProgrammingLanguage("PHP", "Orientação a Objetos"),
+    ProgrammingLanguage("Python", "Orientação a Objetos"),
+    ProgrammingLanguage("C#", "Orientação a Objetos")
+)
---
+++ app/src/main/java/com/example/mergeadapterrecyclerviewtutorial/Tip.kt
@@ -0,0 +1,9 @@
+package com.example.mergeadapterrecyclerviewtutorial
+
+data class Tip(
+    val description: String
+)
+
+val tips = listOf(
+    Tip("Quer aprender a desenvolver aplicativos para a plataforma Android com a linguagem Kotlin? Acesse agora mesmo o YouTube do Douglas Motta para encontrar conteúdos sobre o assunto!")
+)
